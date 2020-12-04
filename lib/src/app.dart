e4a9bdf1511d6650e0e9f3580d3da508c51050a7
Chulwoo Park <park@chulwoo.dev>
2020-12-04 23:10:20 +0900

    chore: initial commit

---
+++ lib/src/app.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class PokedexApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
