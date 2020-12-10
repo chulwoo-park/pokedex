7a8a60a5d75e3b855118a41a102dd955ea5c130d
Chulwoo Park <park@chulwoo.dev>
2020-12-10 10:01:34 +0900

    feat: add OverScrollBackdrop widget

--- lib/src/presentation/screen/home/home.dart
+++ lib/src/presentation/screen/home/home.dart
@@ -5,6 +5,7 @@ import 'package:flutter/widgets.dart';
 
 import '../../resources.dart';
 import '../../route/model.dart';
+import '../widget/overscroll_backdrop.dart';
 import '../widget/search_bar.dart';
 import 'widget/category_card.dart';
 
@@ -25,142 +26,148 @@ class HomeScreen extends StatelessWidget {
 
     return AnnotatedRegion(
       value: SystemUiOverlayStyle.dark,
-      child: Scaffold(
+      child: OverScrollBackdrop(
         backgroundColor: R.color.white,
-        body: CustomScrollView(
-          slivers: [
-            SliverPadding(
-              padding: horizontalPadding.copyWith(top: 117.0),
-              sliver: SliverToBoxAdapter(
-                child: Stack(
-                  clipBehavior: Clip.none,
-                  children: [
-                    Positioned(
-                      right: -100.0,
-                      top: -170.0,
-                      child: Opacity(
-                        opacity: 0.05,
-                        child: ImageIcon(
-                          AssetImage(
-                            R.image.pokeball,
-                          ),
-                          size: 250.0,
-                        ),
-                      ),
-                    ),
-                    Text(
-                      'What pokemon\nare you looking for?',
-                      style: TextStyle(
-                        height: 1.5,
-                        fontSize: 30.0,
-                        fontWeight: FontWeight.w900,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            SliverPadding(
-              padding: horizontalPadding + EdgeInsets.symmetric(vertical: 40.0),
-              sliver: SliverToBoxAdapter(
-                child: SearchBar(),
-              ),
-            ),
-            SliverPadding(
-              padding: horizontalPadding,
-              sliver: SliverGrid(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount:
-                      MediaQuery.of(context).size.width > 720 ? 3 : 2,
-                  mainAxisSpacing: 15.0,
-                  crossAxisSpacing: 10.0,
-                  childAspectRatio: 2.58,
-                ),
-                delegate: SliverChildListDelegate(
-                  [
-                    CategoryCard(
-                      label: 'Pokedex',
-                      color: R.color.puertoRico,
-                      shadowColor: R.color.shamrock,
-                    ),
-                    CategoryCard(
-                      label: 'Moves',
-                      color: R.color.salmon,
-                      shadowColor: R.color.bittersweet,
-                    ),
-                    CategoryCard(
-                      label: 'Abilities',
-                      color: R.color.malibu,
-                      shadowColor: R.color.summerSky,
-                    ),
-                    CategoryCard(
-                      label: 'Items',
-                      color: R.color.kournikova,
-                      shadowColor: R.color.creamCan,
-                    ),
-                    CategoryCard(
-                      label: 'Locations',
-                      color: R.color.affair,
-                      shadowColor: R.color.deepLilac,
-                    ),
-                    CategoryCard(
-                      label: 'Type Charts',
-                      color: R.color.coralTree,
-                      shadowColor: R.color.myPink,
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            SliverToBoxAdapter(
-              child: Stack(
-                children: [
-                  Container(
-                    height: 60.0,
-                    color: R.color.whisper,
-                  ),
-                  Container(
-                    height: 60.0,
-                    decoration: BoxDecoration(
-                      color: R.color.white,
-                      borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(30.0),
-                        bottomRight: Radius.circular(30.0),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            SliverFillRemaining(
-              hasScrollBody: false,
-              child: Container(
-                color: R.color.whisper,
-                padding: horizontalPadding.copyWith(top: 40.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Pokémon News',
-                      style: TextStyle(
-                        fontWeight: FontWeight.w900,
-                        fontSize: 20.0,
-                      ),
-                    ),
-                    Center(
-                      child: Padding(
-                        padding: EdgeInsets.only(top: 60.0),
+        topColor: R.color.white,
+        bottomColor: R.color.whisper,
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          body: CustomScrollView(
+            slivers: [
+              SliverPadding(
+                padding: horizontalPadding.copyWith(top: 117.0),
+                sliver: SliverToBoxAdapter(
+                  child: Stack(
+                    clipBehavior: Clip.none,
+                    children: [
+                      Positioned(
+                        right: -100.0,
+                        top: -170.0,
                         child: Opacity(
-                          opacity: 0.6,
-                          child: Text('There is no news today.'),
+                          opacity: 0.05,
+                          child: ImageIcon(
+                            AssetImage(
+                              R.image.pokeball,
+                            ),
+                            size: 250.0,
+                          ),
                         ),
                       ),
-                    )
+                      Text(
+                        'What pokemon\nare you looking for?',
+                        style: TextStyle(
+                          height: 1.5,
+                          fontSize: 30.0,
+                          fontWeight: FontWeight.w900,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SliverPadding(
+                padding:
+                    horizontalPadding + EdgeInsets.symmetric(vertical: 40.0),
+                sliver: SliverToBoxAdapter(
+                  child: SearchBar(),
+                ),
+              ),
+              SliverPadding(
+                padding: horizontalPadding,
+                sliver: SliverGrid(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount:
+                        MediaQuery.of(context).size.width > 720 ? 3 : 2,
+                    mainAxisSpacing: 15.0,
+                    crossAxisSpacing: 10.0,
+                    childAspectRatio: 2.58,
+                  ),
+                  delegate: SliverChildListDelegate(
+                    [
+                      CategoryCard(
+                        label: 'Pokedex',
+                        color: R.color.puertoRico,
+                        shadowColor: R.color.shamrock,
+                      ),
+                      CategoryCard(
+                        label: 'Moves',
+                        color: R.color.salmon,
+                        shadowColor: R.color.bittersweet,
+                      ),
+                      CategoryCard(
+                        label: 'Abilities',
+                        color: R.color.malibu,
+                        shadowColor: R.color.summerSky,
+                      ),
+                      CategoryCard(
+                        label: 'Items',
+                        color: R.color.kournikova,
+                        shadowColor: R.color.creamCan,
+                      ),
+                      CategoryCard(
+                        label: 'Locations',
+                        color: R.color.affair,
+                        shadowColor: R.color.deepLilac,
+                      ),
+                      CategoryCard(
+                        label: 'Type Charts',
+                        color: R.color.coralTree,
+                        shadowColor: R.color.myPink,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SliverToBoxAdapter(
+                child: Stack(
+                  children: [
+                    Container(
+                      height: 60.0,
+                      color: R.color.whisper,
+                    ),
+                    Container(
+                      height: 60.0,
+                      decoration: BoxDecoration(
+                        color: R.color.white,
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(30.0),
+                          bottomRight: Radius.circular(30.0),
+                        ),
+                      ),
+                    ),
                   ],
                 ),
               ),
-            ),
-          ],
+              SliverFillRemaining(
+                hasScrollBody: false,
+                child: Container(
+                  color: R.color.whisper,
+                  padding: horizontalPadding.copyWith(top: 40.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Pokémon News',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w900,
+                          fontSize: 20.0,
+                        ),
+                      ),
+                      Center(
+                        child: Padding(
+                          padding: EdgeInsets.only(top: 60.0),
+                          child: Opacity(
+                            opacity: 0.6,
+                            child: Text('There is no news today.'),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
